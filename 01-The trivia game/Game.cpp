fd9f53a86d095603850c3e3113a4bc5bb4fa1dd8
fady <algerienvenere@gmail.com>
2023-07-15 20:08:15 +0200

    added at for attribut array

--- 01-The trivia game/Game.cpp
+++ 01-The trivia game/Game.cpp
@@ -79,31 +79,28 @@ const bool& Game::getIsGettingOutOfPenaltyBox() const {
 
 
 
-Game::Game() : places{}, purses{}, currentPlayer(0){
-	for (int i = 0; i < 50; i++)
-	{
-		std::ostringstream oss (std::ostringstream::out);
+Game::Game() : places{}, purses{}, currentPlayer(0) {
+	for (int i = 0; i < 50; i++) {
+		std::ostringstream oss;
 		oss << "Pop Question " << i;
-
 		popQuestions.push_back(oss.str());
 
-		char str[255];
-		sprintf(str, "Science Question %d", i);
-		scienceQuestions.emplace_back(str);
+		std::ostringstream oss1;
+		oss1 << "Science Question " << i;
+		scienceQuestions.emplace_back(oss1.str());
 
-		char str1[255];
-		sprintf(str1, "Sports Question %d", i);
-		sportsQuestions.emplace_back(str1);
+		std::ostringstream oss2;
+		oss2 << "Sports Question " << i;
+		sportsQuestions.emplace_back(oss2.str());
 
 		rockQuestions.push_back(createRockQuestion(i));
 	}
 }
 
-std::string Game::createRockQuestion(int index)
-{
-	char indexStr[127];
-	sprintf(indexStr, "Rock Question %d", index);
-	return indexStr;
+std::string Game::createRockQuestion(int index) {
+	std::ostringstream oss;
+	oss << "Rock Question " << index;
+	return oss.str();
 }
 int Game::howManyPlayers()
 {
@@ -118,9 +115,9 @@ bool Game::isPlayable()
 bool Game::add(std::string playerName){
 	players.push_back(playerName);
 	int players_num = howManyPlayers();
-	places[players_num] = 0;
-	purses[players_num] = 0;
-	inPenaltyBox[players_num] = false;
+	places.at(players_num) = 0;
+	purses.at(players_num) = 0;
+	inPenaltyBox.at(players_num) = false;
 
 	std::cout << playerName << " was added" << std::endl;
 	std::cout << "They are player number " << players.size() << std::endl;
@@ -132,7 +129,7 @@ void Game::roll(int roll)
     std::cout << players[currentPlayer] << " is the current player" << std::endl;
     std::cout << "They have rolled a " << roll << std::endl;
 
-    if (inPenaltyBox[currentPlayer])
+    if (inPenaltyBox.at(currentPlayer))
     {
         if (roll % 2 != 0)
         {
@@ -147,11 +144,11 @@ void Game::roll(int roll)
         }
     }
 
-    places[currentPlayer] = places[currentPlayer] + roll;
-    if (places[currentPlayer] > 11)
-        places[currentPlayer] = places[currentPlayer] - 12;
+    places.at(currentPlayer) = places.at(currentPlayer) + roll;
+    if (places.at(currentPlayer) > 11)
+        places.at(currentPlayer) = places.at(currentPlayer) - 12;
 
-    std::cout << players[currentPlayer] << "'s new location is " << places[currentPlayer] << std::endl;
+    std::cout << players[currentPlayer] << "'s new location is " << places.at(currentPlayer) << std::endl;
     std::cout << "The category is " << currentCategory() << std::endl;
     askQuestion();
 }
@@ -190,7 +187,7 @@ void Game::askQuestion()
 
 std::string Game::currentCategory()
 {
-    int currentPlace = places[currentPlayer];
+    int currentPlace = places.at(currentPlayer);
 
     if (currentPlace == 0 || currentPlace == 4 || currentPlace == 8)
         return "Pop";
@@ -204,7 +201,7 @@ std::string Game::currentCategory()
 
 bool Game::wasCorrectlyAnswered()
 {
-    if (inPenaltyBox[currentPlayer])
+    if (inPenaltyBox.at(currentPlayer))
     {
         if (isGettingOutOfPenaltyBox)
         {
@@ -232,7 +229,7 @@ bool Game::wrongAnswer()
 {
     std::cout << "Question was incorrectly answered" << std::endl;
     std::cout << players[currentPlayer] + " was sent to the penalty box" << std::endl;
-    inPenaltyBox[currentPlayer] = true;
+    inPenaltyBox.at(currentPlayer) = true;
 
     updateCurrentPlayer();
     return true;
@@ -240,14 +237,14 @@ bool Game::wrongAnswer()
 
 bool Game::didPlayerWin()
 {
-    return !(purses[currentPlayer] == 6);
+    return !(purses.at(currentPlayer) == 6);
 }
 
 void Game::incrementPlayerScore()
 {
     std::cout << "Answer was correct!!!!" << std::endl;
-    purses[currentPlayer]++;
-    std::cout << players[currentPlayer] << " now has " << purses[currentPlayer] << " Gold Coins." << std::endl;
+    purses.at(currentPlayer)++;
+    std::cout << players[currentPlayer] << " now has " << purses.at(currentPlayer) << " Gold Coins." << std::endl;
 }
 
 void Game::updateCurrentPlayer()
